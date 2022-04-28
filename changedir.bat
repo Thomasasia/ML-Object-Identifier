56a38591ab1b6fb560bf48acbf38d206e86ffd94
thomasasia <thomas.harriett@gmail.com>
2022-04-27 20:29:13 -0400

    changing directory names

--- changedir.bat
+++ changedir.bat
@@ -1,14 +1,14 @@
-mv images/cats images/cat
+move images/cats images/cat
 git add .
 git commit -m "changing directory names"
 git push
 
-mv images/dogs images/dog
+move images/dogs images/dog
 git add .
 git commit -m "changing directory names"
 git push
 
-mv images/snails images/snail
+move images/snails images/snail
 git add .
 git commit -m "changing directory names"
 git push
